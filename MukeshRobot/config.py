4650202935b9da3aa79964d36c839512f0116d1f
massom0097773 <132870020+massom0097773@users.noreply.github.com>
2023-08-07 04:32:48 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "6246716406" # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = "6141854768" # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "innocentpapabolte"
-    SUPPORT_CHAT = "innocentop"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "innocent_world_chat"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001903923793
     )  # Prints any new group the bot is added to, prints just the name and ID.
